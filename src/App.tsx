cb88533e910681ad93cf6f075ab2d557778f0fb4
Shemona Singh <singhshemona@gmail.com>
2023-08-20 21:46:55 +0200

    updated types

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import './App.css';
 import Sketch from 'react-p5';
+import P5 from 'p5';
 
 const ml5 = require('ml5');
 
 function App() {
-  let classifier: any;
+  let classifier: { classify: (arg0: any, arg1: (error: Error, results: [{ label: ""; }]) => void) => void; };
   let imageModelURL = 'https://teachablemachine.withgoogle.com/models/wZm28bHVl/';
-  let video: any;
-  let flippedVideo: any;
+  let video: P5.Element;
+  let flippedVideo: P5.Element | P5.Image;
   let label = "";
 
   const preload = () => {
     classifier = ml5.imageClassifier(imageModelURL + 'model.json');
   }
 
-  const setup = (p5: any, canvasParentRef: any) => {
+  const setup = (p5: P5, canvasParentRef: any) => {
     ml5.p5Utils.setP5Instance(p5);
     p5.createCanvas(320, 260).parent(canvasParentRef);
     video = p5.createCapture(p5.VIDEO);
@@ -24,13 +25,13 @@ function App() {
     classifyVideo();
   }
 
-  const draw = (p5: any) => {
+  const draw = (p5: P5) => {
     p5.background(0);
     p5.image(flippedVideo, 0, 0);
 
     p5.fill(255);
     p5.textSize(16);
-    p5.textAlign('CENTER');
+    p5.textAlign(p5.CENTER);
     p5.text(label, 100, 100);
   }
 
@@ -39,11 +40,8 @@ function App() {
     classifier.classify(flippedVideo, gotResult);
   }
   
-  function gotResult(error: any, results: any) {
-    if (error) {
-      console.error(error);
-      return;
-    }
+  function gotResult(error: Error, results: [{ label: '' }]) {
+    if (error) throw(error);
 
     console.log(results);
     label = results[0].label;
